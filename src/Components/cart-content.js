9a7fa7d78aea2039c3246b4c58dfe9273db79c67
hoai bao <nguyenthihoaibn96@gmail.com>
2020-12-28 16:15:19 +0700

    edit icon

--- src/Components/cart-content.js
+++ src/Components/cart-content.js
@@ -33,20 +33,7 @@ const CartItems = () => {
                                 defaultValue={2}
                             />
                         </div>
-                        <div className="remove">
-                            <svg
-                                version="1.1"
-                                className="close"
-                                xmlns="//www.w3.org/2000/svg"
-                                xmlnsXlink="//www.w3.org/1999/xlink"
-                                x="0px"
-                                y="0px"
-                                viewBox="0 0 60 60"
-                                enableBackground="new 0 0 60 60"
-                                xmlSpace="preserve"
-                            />
-                            <polygon points="38.936,23.561 36.814,21.439 30.562,27.691 24.311,21.439 22.189,23.561 28.441,29.812 22.189,36.064 24.311,38.186 30.562,31.934 36.814,38.186 38.936,36.064 32.684,29.812" />
-                        </div>
+                        <div className="remove">X</div>
                     </div>
                 </li>
                 <li className="row">
@@ -78,20 +65,7 @@ const CartItems = () => {
                                 defaultValue={2}
                             />
                         </div>
-                        <div className="remove">
-                            <svg
-                                version="1.1"
-                                className="close"
-                                xmlns="//www.w3.org/2000/svg"
-                                xmlnsXlink="//www.w3.org/1999/xlink"
-                                x="0px"
-                                y="0px"
-                                viewBox="0 0 60 60"
-                                enableBackground="new 0 0 60 60"
-                                xmlSpace="preserve"
-                            />
-                            <polygon points="38.936,23.561 36.814,21.439 30.562,27.691 24.311,21.439 22.189,23.561 28.441,29.812 22.189,36.064 24.311,38.186 30.562,31.934 36.814,38.186 38.936,36.064 32.684,29.812" />
-                        </div>
+                        <div className="remove">X</div>
                     </div>
                 </li>
             </ul>
